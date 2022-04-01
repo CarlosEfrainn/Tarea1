ac9ff5907b16b857fc4c45e8097e0c4f93f531f1
Efrain <carlosefra02@gmail.com>
2022-03-31 20:32:08 -0600

    tarea1

---
+++ tarea1/tarea1/EmpleadoAsalariado .cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea1
+{
+    public class EmpleadoAsalariado : Empleado
+    {
+        private decimal salarioSemanal;
+        private decimal salario;
+
+        public EmpleadoAsalariado(string nombre, string apellido, string nss,
+            decimal salarioSemanal) : base(nombre, apellido, nss)
+        {
+            SalarioSemanal = salario;
+        }
+        public decimal SalarioSemanal
+        {
+            get 
+            { 
+                return salarioSemanal;
+            }
+        set
+            {
+                salarioSemanal = ((value >= 0) ? value : 0);
+            }
+        }
+        public override decimal Ingresos()
+        {
+            return SalarioSemanal;
+        }
+        public override string ToString()
+        {
+            return string.Format("empleado asalariado: {0}\n{1}: {2:C}",
+                base.ToString(), "salario semanal", SalarioSemanal);
+
+        }
+    }
+}
---
+++ tarea1/tarea1/EmpleadoPorComision.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tarea1
+{
+    public class EmpleadoPorComision : Empleado
+    {
+        private decimal ventasBrutas;
+        private decimal tarifaComision;
+        public EmpleadoPorComision(string nombre, string apellido, string nss,
+            decimal ventas, decimal tarifa) : base(nombre, apellido, nss)
+        {
+            VentasBrutas = ventas;
+            tarifaComision = tarifa;
+        }
+        public decimal TarifaComision
+        {
+            get
+            {
+                return tarifaComision;
+            }
+            set
+            {
+                tarifaComision = value;
+            }
+        }
+        public decimal VentasBrutas
+        {
+            get
+            {
+                return ventasBrutas;
+            }
+            set
+            {
+                ventasBrutas = (value >= 0) ? value : 0;
+            }
+        }
+        public override decimal Ingresos()
+        {
+            return TarifaComision * VentasBrutas;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}: {1}\n{2}: {3:C}\n{4}: {5:F2}",
+                "empleado por comisión", base.ToString(),
+                "ventas brutas", VentasBrutas, "tarifa de comisión", TarifaComision);
+        }
+    }
+}
